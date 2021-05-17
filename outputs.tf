61570c10e2e7d7f33189b1f2dc14f8af262e847c
Fabian Fischer <fabian.fischer@siemens.com>
2021-05-17 10:50:17 +0200

    fix: update output parameters to new version

--- outputs.tf
+++ outputs.tf
@@ -15,27 +15,27 @@ output "lambda_iam_role_name" {
 
 output "notify_teams_lambda_function_arn" {
   description = "The ARN of the Lambda function"
-  value       = module.lambda.this_lambda_function_arn
+  value       = module.lambda.lambda_function_arn
 }
 
 output "notify_teams_lambda_function_name" {
   description = "The name of the Lambda function"
-  value       = module.lambda.this_lambda_function_name
+  value       = module.lambda.lambda_function_name
 }
 
 output "notify_teams_lambda_function_invoke_arn" {
   description = "The ARN to be used for invoking Lambda function from API Gateway"
-  value       = module.lambda.this_lambda_function_invoke_arn
+  value       = module.lambda.lambda_function_invoke_arn
 }
 
 output "notify_teams_lambda_function_last_modified" {
   description = "The date Lambda function was last modified"
-  value       = module.lambda.this_lambda_function_last_modified
+  value       = module.lambda.lambda_function_last_modified
 }
 
 output "notify_teams_lambda_function_version" {
   description = "Latest published version of your Lambda function"
-  value       = module.lambda.this_lambda_function_version
+  value       = module.lambda.lambda_function_version
 }
 
 output "lambda_cloudwatch_log_group_arn" {
